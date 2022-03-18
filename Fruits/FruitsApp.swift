b8bc38939329c552ac4c8b9e074809de4300ecf4
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-03-18 18:23:21 +0100

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Sarika on 18.03.22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

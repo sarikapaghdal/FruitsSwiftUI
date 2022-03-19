8df787536e67db19ee8de9ecc842551c0ed16460
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-03-19 18:37:08 +0100

    adding card view background

---
+++ Fruits/Views/FruitCardView.swift
@@ -0,0 +1,55 @@
+//
+//  FruitCardView.swift
+//  Fruits
+//
+//  Created by Sarika on 19.03.22.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    
+    //MARK: PROPERTIES
+    
+    
+    //,MARK: BODY
+    
+    var body: some View {
+        ZStack {
+            
+            VStack(spacing : 20) {
+                // FRUIT IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0 , opacity: 0.15), radius: 8, x: 6, y: 8)
+                // FRUIT TITLE
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0), radius: 2, x: 2, y: 2)
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over thr world")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                //BUTTON: START
+                
+            }//: VSTACK
+        }//: ZSTACK
+        
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+       .background(LinearGradient(colors: [Color("ColorBlueBerryLight"), Color("ColorBlueBerryDark")], startPoint: .top, endPoint: .bottom))
+       .cornerRadius(20)
+    }
+}
+
+//MARK: PREVIEW
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
---
+++ Fruits/Views/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  Fruits
+//
+//  Created by Sarika on 19.03.22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}

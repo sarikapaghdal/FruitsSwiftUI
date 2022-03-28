0d1ef33cbc7186e4e1f7420ad5d3750e5ba699a0
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-03-28 16:17:40 +0200

    added source link view with group box

--- Fruits/App/FruitsDetailView.swift
+++ Fruits/App/FruitsDetailView.swift
@@ -13,13 +13,45 @@ struct FruitsDetailView: View {
         NavigationView {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack (alignment: .center, spacing: 20){
+                    
+                    //HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
                     VStack (alignment: .leading, spacing: 20){
+                        //TITLE
                         Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        //HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        //NUTRIENTS
+                        
+                        //SUBHEADLINE
+                        
+                        Text("Learn more about \(fruit.title)".uppercased())
+                        
+                        //DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        //LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
                     }
                     .padding(.horizontal, 20)
                     .frame(maxWidth: 640, alignment: .center)
                 }//: VSTACK
+                .navigationTitle(fruit.title)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
             }//: SCROLLVIEW
+            .edgesIgnoringSafeArea(.top)
         }//: NAVIGATION
     }
 }
